0e5c81bc9577d56beabde2e51e52df1a9937af83
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-27 13:57:40 -0300

    se agrega el titulo, el footer y estilos en app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Container } from 'react-bootstrap';
 function App() {
 
   return (
     <>
+    <Container id='containerPrincipal'>
+      <h1 className='text-center display-2'>Lista de empleados</h1>
+    
+    </Container>
+    <footer className='bg-dark text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
